0b6df3662c7f183711c8368e76f04b9d35f28322
tamasmeidlinger <meidlinger.tamas1@gmail.com>
2025-10-25 02:04:46 +0200

    skeleton2

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -30,7 +30,7 @@ export default async function Page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <Suspense>
+        <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
